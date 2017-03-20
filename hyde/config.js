e27995acada8f581e703cfaf7fa852a9ed800520
Simon Hermann <mail@simonhermann.de>
2017-03-20 17:47:45 +0100

    adding hyde

---
+++ hyde/config.js
@@ -0,0 +1,5 @@
+//Place here your personal Access Token, you can go to our Readme if you need help
+	var AccessToken = '4d445d0314e06002c3f623b8522ab0e75f126c0c';
+
+//Html tab size
+	var htmlEditorTabSize = '2';
